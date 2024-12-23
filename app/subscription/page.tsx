1da0adefc3523c9c2240726adb225280b0a0b0e9
Massu <massu.queirantes@yahoo.com.br>
2024-12-23 16:39:37 -0300

    chore: add prisma and database setup

---
+++ app/subscription/page.tsx
@@ -0,0 +1,6 @@
+const  Subscription = () => {
+    return <h1>Subscription Page</h1>
+
+};
+
+export default Subscription ;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,6 @@
+const  Transactions = () => {
+    return <h1>Transactions Page</h1>
+
+};
+
+export default Transactions ;
