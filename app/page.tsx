ead780ee6281b7e1e1e36bb5d5cdfdcd7db1a283
Massu <massu.queirantes@yahoo.com.br>
2024-12-23 17:00:50 -0300

    chore: add taillwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-const Home = () =>{
-  return <h1>Hello World</h1>
+const Home = () => {
+  return (
+    <div className="flex w-screen items-center justify-center">
+      Hello World
+      <h1 className="p-5 text-red-500">Hello World</h1>
+    </div>
+  );
 };
 
 export default Home;
