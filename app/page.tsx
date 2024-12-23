6cb604890354d7c521681bb91d1a6f6b8db487ee
Massu <massu.queirantes@yahoo.com.br>
2024-12-23 17:32:39 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return (
-    <div className="flex w-screen items-center justify-center">
-      Hello World
-      <h1 className="p-5 text-red-500">Hello World</h1>
-    </div>
-  );
+  return <Button>Hello brazil</Button>;
 };
 
 export default Home;
