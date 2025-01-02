8c9ab7b16ff5ded44dbd585ebe3a5b02cc0a013b
Massu <massu.queirantes@yahoo.com.br>
2025-01-02 16:50:43 -0300

    feat: add badge 1

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,25 @@
-const TransactionsPage = () => {
-  return <h1>Transactions Page</h1>;
+import { Button } from "@/components/ui/button";
+import { db } from "../_lib/prisma";
+import { ArrowDownUpIcon } from "lucide-react";
+import { DataTable } from "@/components/data-table";
+import { transactionColumns } from "./_columns";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+
+  return (
+    <div className="space-y-6 p-6">
+      {/**Titulo e botão */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          <ArrowDownUpIcon />
+          Adicionar transação
+        </Button>
+      </div>
+      <DataTable columns={transactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default TransactionsPage;
