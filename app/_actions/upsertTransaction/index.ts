86a7c0a4ccada3839c4324a71b4722eb42dcac98
Massu <massu.queirantes@yahoo.com.br>
2025-01-29 20:07:41 -0300

    fix: upsert transaction function

---
+++ app/_actions/upsertTransaction/index.ts
@@ -0,0 +1,40 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import {
+  TransactionCategory,
+  TransactionPaymentsMethod,
+  TransactionType,
+} from "@prisma/client";
+import { upsertTransactionSchema } from "./schema";
+import { revalidatePath } from "next/cache";
+
+interface UpsertTransactionParams {
+  id?: string;
+  name: string;
+  amount: number;
+  type: TransactionType;
+  category: TransactionCategory;
+  paymentsMethod: TransactionPaymentsMethod;
+  date: Date;
+}
+
+export const upsertTransaction = async (params: UpsertTransactionParams) => {
+  upsertTransactionSchema.parse(params);
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  await db.transaction.upsert({
+    update: { ...params, userId },
+    create: { ...params, userId },
+    where: {
+      id: params?.id ?? "",
+    },
+  });
+
+  revalidatePath("/transactions");
+};
